7d932a3585dfdd3e8d6f4c9cb773de7f71598e44
LorakV9 <121059447+LorakV9@users.noreply.github.com>
2024-12-30 11:48:39 +0100

    v0.1.Produkty

--- Program.cs
+++ Program.cs
@@ -9,11 +9,17 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
     new MySqlServerVersion(new Version(8, 0, 33))));
 
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();  // Interfejs u¿ytkownika Swagger
+}
 app.UseRouting();
 
 app.UseEndpoints(endpoints =>
